55dfcca05fc9122b09260983a093a7bd193ca161
Furkan Kaya <furkan.kaya2@obss.com.tr>
2022-01-10 02:58:14 +0300

    vue-eshop-layout

--- vue-basics/src/components/Student.vue
+++ vue-basics/src/components/Student.vue
@@ -21,7 +21,7 @@ export default {
   methods: {
     showScore() {
       this.$emit('showScore', this.data.score)
-      }
+    }
   }
 }
 </script>
--- vue-e-shop-layout/src/utils/api.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const BASE_URL = 'https://my-json-server.typicode.com/modanisa/bootcamp-db';
-const API_URL = Object.freeze({
-    productList: `${BASE_URL}/products`
-});
-
-export const getProductList = () => axios.get(API_URL.productList).then(response => response.data);
